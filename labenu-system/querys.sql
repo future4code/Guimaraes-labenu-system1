95380e96575bf845522c04eacb833c795d987091
raulrita08 <raul.vinicius@live.com>
2022-07-19 18:57:46 -0300

    organizando banco e classes

--- labenu-system/querys.sql
+++ labenu-system/querys.sql
@@ -1,3 +1,4 @@
+-- Active: 1657909499156@@35.226.146.116@3306@guimaraes-4211089-raul-rita
 USE `guimaraes-4211089-raul-rita`;
 
 CREATE TABLE LS_Turma(
@@ -19,6 +20,10 @@ CREATE TABLE LS_Hobby (
 	nome_hobby ENUM("assistir séries de tv", "jogar videogames", "sair com os amigos", "praticar esportes") NOT NULL
 );
 
+ALTER TABLE `LS_Hobby`
+MODIFY COLUMN nome_hobby VARCHAR(255) NOT NULL;
+SELECT * FROM `LS_Hobby`;
+
 CREATE TABLE LS_Hobby_Estudante(
 	id VARCHAR(255) PRIMARY KEY,
 	id_estudante VARCHAR(255), FOREIGN KEY(id_estudante) REFERENCES LS_Estudante(id),
@@ -26,3 +31,27 @@ CREATE TABLE LS_Hobby_Estudante(
 );
 SELECT * FROM `LS_Hobby_Estudante`;
 
+CREATE TABLE LS_Docente(
+	id VARCHAR(255) PRIMARY KEY,
+	nome VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL UNIQUE, 
+	data_nasc VARCHAR(255) NOT NULL,
+	turma_id VARCHAR(255), FOREIGN KEY (turma_id) REFERENCES LS_Turma(id)
+);
+
+SELECT * FROM `LS_Docente_Especialidade`;
+
+CREATE TABLE LS_Especialidade (
+	id VARCHAR(255) PRIMARY KEY,
+	nome_especialidade ENUM("JS", "CSS", "React", "Typescript", "POO") NOT NULL
+);
+
+ALTER TABLE `LS_Especialidade`
+MODIFY COLUMN nome_especialidade VARCHAR(255) NOT NULL;
+SELECT * FROM `LS_Especialidade`;
+
+CREATE TABLE LS_Docente_Especialidade (
+	id VARCHAR(255) PRIMARY KEY,
+	id_docente VARCHAR(255), FOREIGN KEY(id_docente) REFERENCES LS_Docente(id),
+	id_especialidade VARCHAR(255), FOREIGN KEY(id_especialidade) REFERENCES LS_Especialidade(id)
+);
